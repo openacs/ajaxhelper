e492a6b1b2460f374dca113c78bda4f7060c5f88
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2019-08-21 06:47:49 +0000

    follow current HTML recommendations

--- www/doc/blank-master.tcl
+++ www/doc/blank-master.tcl
@@ -60,7 +60,7 @@ ad_page_contract {
 }
 
 if {![info exists doc(type)]} { 
-    set doc(type) {<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">}
+    set doc(type) {<!DOCTYPE HTML>}
 }
 
 if {![info exists doc(title)]} {
